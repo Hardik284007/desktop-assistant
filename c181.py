915e012243597b6b1a29d342594df323b19e525a
Hardik284007 <78480267+Hardik284007@users.noreply.github.com>
2023-02-07 18:56:46 +0530

    Add files via upload

--- c181.py
+++ c181.py
@@ -1,35 +1,34 @@
-from tkinter import*
+from tkinter import *
 import speech_recognition as sr
+import webbrowser
 import pyttsx3
 from datetime import datetime
-import webbrowser
 import subprocess
+
+root = Tk()
+root.geometry("500x500")
+root.configure(background="light blue")
+
+label = Label(root, text="Welcome to your personal desktop assistant", bg="light blue", font=("Bahnschrift Light", 15, "bold"))
+label.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 
 text_to_speech = pyttsx3.init()
 
 def speak(audio):
     text_to_speech.say(audio)
     text_to_speech.runAndWait()
-    
-root = Tk()
-root.geometry("500x500")
-root.config(background="light blue")
-
-label = Label(root, text="Welcome to your personal desktop assistant", bg="light blue", font=("Bahnschrift Light", 15, "bold"))
-label.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 
 def r_audio():
-    speak("How can i help you?")
     speech_recognisor = sr.Recognizer()
+    speak("How can i help you?")
     with sr.Microphone() as source:
-        audio=  speech_recognisor.listen(source)
+        audio= speech_recognisor.listen(source)
         voice_data=''
         try:
             voice_data=  speech_recognisor.recognize_google(audio, language='en-in')
         except sr.UnknownValueError:
             print('Please repeat I did not get that')
             speak('Please repeat I did not get that')
-
     respond(voice_data)
         
 def respond(voice_data):
@@ -49,14 +48,18 @@ def respond(voice_data):
     if "search" in voice_data :
         speak("Opening Google")
         print("Opening Google")
+        webbrowser.get().open("https://google.com")
         
     if "video" in voice_data :
         speak("Opening Youtube")
-        print("Opening Youtube")        
+        print("Opening Youtube")      
+        webbrowser.get().open("https://youtube.com")
+
 
     if "text editor" in voice_data :
         speak("Opening the app")
         print("Opening the app")    
+        subprocess.Popen(["notepad.exe"])
     
 r_audio()
 
